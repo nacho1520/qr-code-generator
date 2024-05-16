d601a4d31752ed459124c6bf5fabbbe9fc877ab2
Ignacio Lopez <ignaciolopestela12@gmail.com>
2024-05-16 12:19:13 -0300

    Download functionality

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ window.addEventListener('load', () => {
     const mainPage = document.querySelector('.main-container');
     const submitBtn = document.querySelector('#submit-btn');
     const qrPage = document.querySelector('.qr-page');
+    const downloadBtn = document.querySelector('#download-btn');
 
     const generateQrCode = (url) => {
         const qrCode = new QRCode(document.querySelector('.qr-code'), {
@@ -16,6 +17,18 @@ window.addEventListener('load', () => {
         const url = document.querySelector('#form-data').value;
         generateQrCode(url);
         qrPage.classList.add('active');
+    });
+
+    downloadBtn.addEventListener('click', () => {
+        const qrImage = document.querySelector('.qr-img');
+        html2canvas(qrImage).then((canvas) => {
+            const base64Image = canvas.toDataURL("image/png");
+            var anchor = document.createElement('a');
+            anchor.setAttribute("href", base64Image);
+            anchor.setAttribute("download", "qr-code.png");
+            anchor.click();
+            anchor.remove();
+        });
     });
     
 });
